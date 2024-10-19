b13a759cb3a3ae9f6a0398b36af6fd5d1b35531e
HunterUMA96 <hunter.connell@maine.edu>
2024-10-19 13:06:10 -0400

    Commit for week 5 assignments.

---
+++ Week5/bash-pingScript.sh
@@ -0,0 +1,13 @@
+#!C:\Program Files\Git\bin\sh.exe
+
+#Hunter Joseph Connell 10/17/2024
+# ISS 212 CS Scripting - WK 5 - Bash Review
+# This script will ping any address provided as an argument.
+# usage: bash bash-pingScript.sh <IP Address Here, sans brackets.>
+
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping "${TARGET}"
